2c7845bde1b42b6a74f4be5d8fd3d9fe7ef05eb7
lcgao <lcgao2016@gmail.com>
2019-10-28 18:16:39 +0800

    Add directly to the photo option

--- app/src/main/java/com/robotemi/welcomingbtob/featurelist/play/PlayFragment.kt
+++ app/src/main/java/com/robotemi/welcomingbtob/featurelist/play/PlayFragment.kt
@@ -1,6 +1,9 @@
 package com.robotemi.welcomingbtob.featurelist.play
 
 import android.content.Intent
+import android.text.TextUtils
+import com.robotemi.sdk.NlpResult
+import com.robotemi.sdk.constants.SdkConstants
 import com.robotemi.welcomingbtob.R
 import com.robotemi.welcomingbtob.featurelist.FeatureBaseFragment
 import com.robotemi.welcomingbtob.featurelist.adapter.ViewHolder
@@ -20,9 +23,13 @@ class PlayFragment : FeatureBaseFragment() {
 
     override fun handleAction(featureObj: Any) {
         when (featureObj as String) {
-            getString(R.string.feature_take_photo) -> startSkill(SKILL_PACKAGE_CAMERA)
+            getString(R.string.feature_take_photo) -> {
+                val nlpResult = NlpResult()
+                nlpResult.action = SKILL_CAMERA_ACTION_TAKE_PICTURE
+                startSkill(SKILL_PACKAGE_CAMERA, nlpResult)
+            }
             getString(R.string.feature_follow) -> robot.beWithMe()
-            getString(R.string.feature_play_music) -> startSkill(SKILL_PACKAGE_MUSIC)
+            getString(R.string.feature_play_music) -> startSkill(SKILL_PACKAGE_MUSIC, null)
         }
     }
 
@@ -37,7 +44,7 @@ class PlayFragment : FeatureBaseFragment() {
         setCloseVisibility(false)
     }
 
-    private fun startSkill(packageNameWithoutSuffix: String) {
+    private fun startSkill(packageNameWithoutSuffix: String, nlpResult: NlpResult?) {
         val wakeupWord = robot.wakeupWord
         val packageName = StringBuilder(packageNameWithoutSuffix)
         if (wakeupWord.toLowerCase() == Constants.WAKEUP_WORD_ALEXA.toLowerCase()
@@ -49,6 +56,9 @@ class PlayFragment : FeatureBaseFragment() {
         }
         val intent = context?.packageManager?.getLaunchIntentForPackage(packageName.toString())
         intent?.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_TASK_ON_HOME
+        if (nlpResult != null) {
+            intent?.putExtra(SdkConstants.EXTRA_NLP_RESULT, nlpResult)
+        }
         if (intent == null) {
             return
         }
@@ -59,6 +69,8 @@ class PlayFragment : FeatureBaseFragment() {
         private const val SKILL_PACKAGE_CAMERA = "com.roboteam.teamy.camera"
 
         private const val SKILL_PACKAGE_MUSIC = "com.roboteam.teamy.iheart"
+
+        private const val SKILL_CAMERA_ACTION_TAKE_PICTURE = "camera.take.picture"
 
         fun newInstance() = PlayFragment()
     }
